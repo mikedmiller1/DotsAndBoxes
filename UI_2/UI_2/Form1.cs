d05c510d6ce83ad2791812f12386f6adf0f5e06d
Jay Shah <shahjay702@gmail.com>
2016-11-15 18:02:46 -0500

    Add files via upload

---
+++ UI_2/UI_2/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UI_2
+{
+    public partial class Form1 : Form
+    {
+        Graphics drawArea;
+        Pen myPen = new Pen(Color.Black);
+        int panelSizeX = 0;  // width of panel
+        int panelSizeY = 0;  // length of panel
+        int panelStartX = 0; //top left position of panel
+        int panelStartY = 0; //top left position of panel these are starting points
+        int incrementsX = 0;
+        int incrementsY = 0;
+        int mouseDownPointX = 0;
+        int mouseDownPointY = 0;
+        int mouseUpPointX = 0;
+        int mouseUpPointY = 0;
+        List<Point> points = new List<Point>();
+        public Form1()
+        {
+            InitializeComponent();
+            panelSizeX = panel2.Size.Width;
+            panelSizeY = panel2.Size.Height;
+            panelStartX = panel2.Location.X;
+            panelStartY = panel2.Location.Y;
+            
+            drawArea = panel2.CreateGraphics();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel2_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int rows = Int32.Parse(textBox1.Text); // get rows from user
+            int cols = Int32.Parse(textBox2.Text);
+            incrementsX = (panelSizeX - panelStartX) / rows;
+            incrementsY = (panelSizeY - panelStartY) / cols;
+
+            panel2.Refresh();
+            drawIt(rows,cols);
+
+        }
+
+        private void drawIt(int rows,int cols)
+        {
+            int k = 0;
+            int l = 0;
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    drawArea.DrawRectangle(myPen, k, l, 5, 5);
+                   k= k + incrementsX;
+                    
+                }
+                l = l + incrementsY;
+                k = 0;
+            }
+        }
+
+        private void panel2_MouseDown(object sender, MouseEventArgs e)
+        {
+            mouseDownPointX = e.X;
+            mouseDownPointY = e.Y;
+
+        }
+
+        private void panel2_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void panel2_MouseUp(object sender, MouseEventArgs e)
+        {
+            mouseUpPointX = e.X;
+            mouseUpPointY = e.Y;
+            drawLine(mouseDownPointX, mouseDownPointY, mouseUpPointX, mouseUpPointY);
+        }
+
+        private void drawLine(int downX,int downY, int upX, int upY)
+        {
+            downX = (int)Math.Floor((double)downX);
+            downY = (int)Math.Floor((double)downY);
+            upX = (int)Math.Floor((double)upX);
+            upY = (int)Math.Floor((double)upY);
+
+            Point pointStart = new Point(downX, downY);
+            Point pointFinish = new Point(upX, upY);
+            
+            points.Add(pointStart);  //save points
+            points.Add(pointFinish); //save points
+            drawArea.DrawLine(myPen, pointStart, pointFinish);
+            
+        }
+
+    }
+}
